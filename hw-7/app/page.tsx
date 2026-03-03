171faafdfb69eb3ffc5c1882ba95153ca92749b8
Computer <macbook@MacBook-Pro-khxng-Computer.local>
2026-03-03 19:23:22 +0700

    add hw7

---
+++ hw-7/app/page.tsx
@@ -0,0 +1,48 @@
+
+export default function Home() {
+  return (
+    <div>
+      <hr />
+      <main>
+          <section>
+              <div className="section-1">
+                  <div className="section-1-text">
+                      <div className="section-1-header">
+                          <h4>Lorem ipsum dolor</h4>
+                          <h2>WE ARE THE BEST</h2>
+                      </div>
+                      <p className="section-1-subtext">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum,
+                          veniam? Alias, perferendis
+                          optio, voluptas qui quis illo eum rem voluptate quibusdam, libero in sunt rerum?</p>
+                      <button className="section-1-buttom">Join us now</button>
+                  </div>
+              </div>
+          </section>
+          <hr />
+          <section>
+              <ul className="section-2">
+                  <li>SERVICE 1</li>
+                  <li>SERVICE 2</li>
+                  <li>SERVICE 3</li>
+              </ul>
+          </section>
+          <hr />
+          <section>
+              <div className="section-3">
+                  <div className="section-3-img1"></div>
+                  <div className="section-3-img2"></div>
+                  <div className="section-3-img3"></div>
+                  <div className="section-3-img4"></div>
+              </div>
+          </section>
+      </main>
+      <hr />
+
+      <footer>
+          <h2>Lorem ipsum dolor sit amet</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem recusandae</p>
+          <h3>Copyright C ALL Right Reserved</h3>
+      </footer>
+    </div>
+  );
+}
